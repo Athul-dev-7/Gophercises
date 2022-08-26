ec36308387c0c44cc4eb36c5e827eeeb32ad0dac
Das <athuldas@hamon.in>
2022-08-26 18:36:43 +0530

    Adds router for handling any url.

--- url-shortener/main/main.go
+++ url-shortener/main/main.go
@@ -13,11 +13,26 @@ var pathsToUrls = map[string]string{
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+	router.GET("/", home)
+	router.GET("/:str", redirectPage)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func home(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Welcome to Url-shortener")
+}
+
+func redirectPage(c *gin.Context) {
+	path := c.Request.URL.Path
+	fmt.Println("The URL: ", c.Request.Host+c.Request.URL.Path)
+	fmt.Println("Path : ", c.Request.URL.Path)
+	fmt.Println("Redirecting to : ", pathsToUrls[path])
+	if dest, ok := pathsToUrls[path]; ok {
+		c.Redirect(302, dest)
+	} else {
+		c.Redirect(302, "http://127.0.0.1:8080")
+	}
+
 }
