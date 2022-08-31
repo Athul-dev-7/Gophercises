6e68d54353860a585bdb4c9151958a37b47e8bc7
Das <athuldas@hamon.in>
2022-08-31 16:59:14 +0530

    Opens the JSON file.

--- choose-your-own-adventure/main.go
+++ choose-your-own-adventure/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	fileName := flag.String("file", "gopher.json", "The JSON file with the story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s\n", *fileName)
+
+	file, err := os.Open(*fileName)
+	if err != nil {
+		panic(err)
+	}
 }
