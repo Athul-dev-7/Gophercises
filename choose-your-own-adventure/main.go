1aed1584356be4bcbf8ec6c6cfe035f10b50e9f1
Das <athuldas@hamon.in>
2022-08-31 16:50:20 +0530

    Getting the JSON file.

--- choose-your-own-adventure/main.go
+++ choose-your-own-adventure/main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
+	fileName := flag.String("file", "gopher.json", "The JSON file with the story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s\n", *fileName)
 }
